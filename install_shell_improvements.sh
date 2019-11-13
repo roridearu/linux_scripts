62676b6eede421430ec37b41455663e48c822962
roridearu <roridearu@protonmail.ch>
2019-11-13 23:45:19 +1300

    Add install_shell_improvements.sh

---
+++ install_shell_improvements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
